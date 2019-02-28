a0f8797b40608d444cd58f4e73078d9bad7a7b92
bernardo <epiphonetodie@gmail.com>
2019-02-28 13:52:24 +0100

    initial commit

---
+++ MvcMovie/Controllers/MoviesController.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MvcMovie.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly MvcMovieContext _context;
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public MoviesController(IHostingEnvironment hostingEnvironment, MvcMovieContext context)
+        {
+            _context = context;
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        public ActionResult GetBrowserLanguage(string navigatorlanguage)
+        {
+            try
+            {
+                string localePattern = "lib\\cldr-data\\main\\{0}";
+                var cultureToUse = "es-ES"; //Default regionalisation to use
+                string defaultLanguage = "es";
+
+                if (navigatorlanguage.StartsWith("es", StringComparison.InvariantCultureIgnoreCase))
+                    cultureToUse = "es";
+                if (navigatorlanguage.StartsWith("de", StringComparison.InvariantCultureIgnoreCase))
+                    cultureToUse = "de";
+                if (navigatorlanguage.StartsWith("en", StringComparison.InvariantCultureIgnoreCase))
+                    cultureToUse = "en-US-POSIX";
+
+                if (!System.IO.Directory.Exists(System.IO.Path.Combine(_hostingEnvironment.WebRootPath, string.Format(localePattern, cultureToUse))))
+                    cultureToUse = defaultLanguage;
+
+                return Json(new
+                {
+                    error = false,
+                    exception = false,
+                    language = cultureToUse
+                });
+            }
+            catch (Exception ex)
+            {
+                return Json(new
+                {
+                    error = true,
+                    exception = true,
+                    statusText = ex.Message
+                });
+            }
+        }
+
+        // GET: Movies
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Movie.ToListAsync());
+        }
+
+        // GET: Movies/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movie
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+        // GET: Movies/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Movies/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,ReleaseData,Genre,Price")] Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(movie);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(movie);
+        }
+
+        // GET: Movies/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movie.FindAsync(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            return View(movie);
+        }
+
+        // POST: Movies/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ReleaseData,Genre,Price")] Movie movie)
+        {
+            if (id != movie.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(movie);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MovieExists(movie.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(movie);
+        }
+
+        // GET: Movies/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movie
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+        // POST: Movies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var movie = await _context.Movie.FindAsync(id);
+            _context.Movie.Remove(movie);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MovieExists(int id)
+        {
+            return _context.Movie.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ MvcMovie/gulpfile.js
@@ -0,0 +1,47 @@
+﻿var gulp = require('gulp');
+var del = require('del');
+
+var nodeRoot = './node_modules/';
+var targetPath = './wwwroot/lib/';
+var scriptsPath = './Scripts/';
+
+gulp.task('clean', function () {
+    return del([targetPath + '/**/*']);
+});
+
+gulp.task('default', done => {
+    gulp.src(nodeRoot + "cldrjs/dist/cldr.js").pipe(gulp.dest(targetPath + "/cldrjs/dist"));
+    gulp.src(nodeRoot + "cldrjs/dist/cldr/event.js").pipe(gulp.dest(targetPath + "/cldrjs/dist/cldr"));
+    gulp.src(nodeRoot + "cldrjs/dist/cldr/supplemental.js").pipe(gulp.dest(targetPath + "/cldrjs/dist/cldr"));
+
+    gulp.src(nodeRoot + "cldr-data/supplemental/likelySubtags.json").pipe(gulp.dest(targetPath + "/cldr-data/supplemental"));
+    gulp.src(nodeRoot + "cldr-data/main/es/numbers.json").pipe(gulp.dest(targetPath + "/cldr-data/main/es"));
+    gulp.src(nodeRoot + "cldr-data/main/en-US-POSIX/numbers.json").pipe(gulp.dest(targetPath + "/cldr-data/main/en-US-POSIX"));
+    gulp.src(nodeRoot + "cldr-data/main/de/numbers.json").pipe(gulp.dest(targetPath + "/cldr-data/main/de"));
+    gulp.src(nodeRoot + "cldr-data/supplemental/numberingSystems.json").pipe(gulp.dest(targetPath + "/cldr-data/supplemental"));
+    gulp.src(nodeRoot + "cldr-data/main/es/ca-gregorian.json").pipe(gulp.dest(targetPath + "/cldr-data/main/es"));
+    gulp.src(nodeRoot + "cldr-data/main/en-US-POSIX/ca-gregorian.json").pipe(gulp.dest(targetPath + "/cldr-data/main/en-US-POSIX"));
+    gulp.src(nodeRoot + "cldr-data/main/de/ca-gregorian.json").pipe(gulp.dest(targetPath + "/cldr-data/main/de"));
+    gulp.src(nodeRoot + "cldr-data/main/es/timeZoneNames.json").pipe(gulp.dest(targetPath + "/cldr-data/main/es"));
+    gulp.src(nodeRoot + "cldr-data/main/en-US-POSIX/timeZoneNames.json").pipe(gulp.dest(targetPath + "/cldr-data/main/en-US-POSIX"));
+    gulp.src(nodeRoot + "cldr-data/main/de/timeZoneNames.json").pipe(gulp.dest(targetPath + "/cldr-data/main/de"));
+    gulp.src(nodeRoot + "cldr-data/supplemental/timeData.json").pipe(gulp.dest(targetPath + "/cldr-data/supplemental"));
+    gulp.src(nodeRoot + "cldr-data/supplemental/weekData.json").pipe(gulp.dest(targetPath + "/cldr-data/supplemental"));
+
+
+    gulp.src(nodeRoot + "globalize/dist/globalize.js").pipe(gulp.dest(targetPath + "/globalize/dist"));
+    gulp.src(nodeRoot + "globalize/dist/globalize/number.js").pipe(gulp.dest(targetPath + "/globalize/dist/globalize"));
+    gulp.src(nodeRoot + "globalize/dist/globalize/date.js").pipe(gulp.dest(targetPath + "/globalize/dist/globalize"));
+
+    gulp.src(scriptsPath + "jquery.validation.globalize.1.1.0/Scripts/jquery.validate.globalize.js").pipe(gulp.dest(targetPath + "/jquery-validation-globalize"));
+
+    gulp.src(nodeRoot + "bootstrap/dist/css/bootstrap.css").pipe(gulp.dest(targetPath + "/bootstrap/dist/css"));
+    gulp.src(nodeRoot + "bootstrap/dist/js/bootstrap.bundle.js").pipe(gulp.dest(targetPath + "/bootstrap/dist/js"));
+
+    gulp.src(nodeRoot + "jquery/dist/jquery.js").pipe(gulp.dest(targetPath + "/jquery/dist"));
+
+    gulp.src(nodeRoot + "jquery-validation/dist/jquery.validate.js").pipe(gulp.dest(targetPath + "/jquery-validation/dist"));
+
+    gulp.src(nodeRoot + "jquery-validation-unobtrusive/dist/jquery.validate.unobtrusive.js").pipe(gulp.dest(targetPath + "/jquery-validation-unobtrusive"));
+    done();
+});
